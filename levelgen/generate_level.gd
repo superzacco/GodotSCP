3f52202e2ed544ec66b5422516f5f4cddc4f125d
Zacco <zkmuffingamer@gmail.com>
2024-12-02 03:30:56 -0600

    level generation updated again, rooms are connected now

--- levelgen/generate_level.gd
+++ levelgen/generate_level.gd
@@ -5,7 +5,11 @@ extends Node
 @export var spawnRoom: PackedScene
 
 # HALLWAYS
+@export var twoWayHallways: Array[PackedScene]
+@export var threeWayIntersections: Array[PackedScene]
 @export var fourWayInteresctions: Array[PackedScene]
+
+@export var twoWayBends: Array[PackedScene]
 #endregion
 
 var spaceMultiplier: float = 15
@@ -20,6 +24,14 @@ var hallOffset: int
 
 var mapGrid = []
 var temporaryRooms = []
+
+var numOfSurroundingRooms: int
+var directions: Array = [
+	Vector2(-1, 0),  # left
+	Vector2(1, 0),   # right
+	Vector2(0, -1),  # back
+	Vector2(0, 1)    # forward
+]
 
 func _ready() -> void:
 	for i in mapWidth:
@@ -43,7 +55,7 @@ func generate_map():
 		generate_long_hall(zOffset)
 		zOffset += 3
 	
-	replace_temp_rooms()
+	check_rooms_and_replace()
 
 
 func generate_long_hall(zOffset):
@@ -54,7 +66,7 @@ func generate_long_hall(zOffset):
 	var hallMax: Vector3 = Vector3(0, 0, 0)
 	
 	for x in hallLength:
-		var spawnedRoom: Node3D = spawn_room(fourWayInteresctions[0], x + hallOffset, zOffset)
+		var spawnedRoom: Node3D = spawn_room(fourWayInteresctions[0], x + hallOffset, zOffset, true)
 		
 		#if FIRST
 		if x == 0:
@@ -68,7 +80,7 @@ func generate_long_hall(zOffset):
 
 
 func generate_connecting_halls(hallMin, hallMax):
-	var amountOfHalls: int = randi_range(1, 4)
+	var amountOfHalls: int = randi_range(2, 4)
 	
 	var startingPoint = hallMin.x / 15
 	var endingPoint = hallMax.x / 15
@@ -77,28 +89,38 @@ func generate_connecting_halls(hallMin, hallMax):
 	var zPosition = hallMin.z / 15
 	
 	for i in amountOfHalls:
-		var distanceAddedBetween: int = randi_range(2,5)
+		var distanceAddedBetween: int = 0
+		
+		if i == 0:
+			distanceAddedBetween = randi_range(0,6)
+		if i > 0:
+			distanceAddedBetween = randi_range(3,7)
+	
 		xPosition += distanceAddedBetween
 		
 		if xPosition > endingPoint:
 			return
 		
 		for i2 in 2:
-			spawn_room(fourWayInteresctions[0], xPosition, zPosition + i2 + 1)
+			spawn_room(fourWayInteresctions[0], xPosition, zPosition + i2 + 1, true)
 	pass
 
 
-func spawn_room(room, x, z):
-	var spawnedRoom: Node3D = room.instantiate()
+func spawn_room(room, x, z, temp: bool = false):
+	var spawnedRoom: Node3D = null
+	
+	if room != null:
+		spawnedRoom = room.instantiate()
+	
+	if temp:
+		temporaryRooms[x][z] = spawnedRoom
 	
 	add_child(spawnedRoom)
-	temporaryRooms[x][z] = spawnedRoom
-	
 	spawnedRoom.position = Vector3(x, 0, z) * spaceMultiplier
 	return spawnedRoom
 
 
-func replace_temp_rooms():
+func check_rooms_and_replace():
 	for x in temporaryRooms.size():
 		for z in temporaryRooms[x].size():
 			if temporaryRooms[x][z] != null:
@@ -106,13 +128,112 @@ func replace_temp_rooms():
 	pass
 
 
-func check_surrounding_rooms(room, x, z):
-	var numOfSurroundingRooms: int
-	
+func check_surrounding_rooms(temporaryRoom, x, z):
+	numOfSurroundingRooms = 0
+
 	var xSize = temporaryRooms.size() - 1
 	var zSize = temporaryRooms[0].size() - 1
 	
-	# chat, code goes here...
+	#for vec2Dir in directions:
+		#var nx = x + vec2Dir.x
+		#var nz = z + vec2Dir.y
+		#if nx >= 0 and nx < xSize and nz >= 0 and nz < zSize:
+			#if temporaryRooms[nx][nz] != null:
+				#numOfSurroundingRooms += 1
 	
-	print(room.name + " has " + str(numOfSurroundingRooms) + " surrounding rooms!")
+	var nx = x - 1
+	var nz = z
+	if nx >= 0 and nx < xSize and temporaryRooms[nx][nz] != null:
+		numOfSurroundingRooms += 1
+	
+	nx = x + 1
+	nz = z
+	if nx >= 0 and nx < xSize and temporaryRooms[nx][nz] != null:
+		numOfSurroundingRooms += 1
+	
+	nx = x
+	nz = z - 1
+	if nz >= 0 and nz < zSize and temporaryRooms[nx][nz] != null:
+		numOfSurroundingRooms += 1
+	
+	nx = x
+	nz = z + 1
+	if nz >= 0 and nz < zSize and temporaryRooms[nx][nz] != null:
+		numOfSurroundingRooms += 1
+	
+	replace_temporary_room(temporaryRoom, numOfSurroundingRooms, x, z)
 	pass
+
+
+func replace_temporary_room(temporaryRoom, surroundingRooms: int, x: int, z: int):
+	
+	var spawnedRoom = null
+	var timesToRotate: int = 0
+	
+	var roomToSpawn: PackedScene = null
+	
+	temporaryRoom.queue_free()
+	match surroundingRooms:
+		0:
+			print_debug("Room has 0 surrounding rooms!")
+		1:
+			roomToSpawn = spawnRoom
+		2:
+			if (temporaryRooms[x+1][z] != null and temporaryRooms[x-1][z] != null):
+				roomToSpawn = twoWayHallways[0]
+				timesToRotate = 1
+			elif (temporaryRooms[x][z+1] != null and temporaryRooms[x][z-1] != null):
+				roomToSpawn = twoWayHallways[0]
+			else:
+				roomToSpawn = twoWayBends[0]
+				
+				# Rotate the Bend
+				if temporaryRooms[x][z+1] != null:
+					if temporaryRooms[x+1][z]:
+						timesToRotate = 2
+						pass
+					
+					if temporaryRooms[x-1][z]:
+						timesToRotate = 3
+						pass
+						
+				if temporaryRooms[x][z-1] != null:
+					if temporaryRooms[x+1][z]:
+						timesToRotate = 1
+						pass
+					
+					if temporaryRooms[x-1][z]:
+						timesToRotate = 0
+						pass
+		3:
+			roomToSpawn = threeWayIntersections[0]
+			
+			if temporaryRooms[x][z+1] != null:
+				timesToRotate = 2
+		4:
+			roomToSpawn = fourWayInteresctions[0]
+	
+	spawnedRoom = spawn_room(roomToSpawn, x, z)
+	
+	if timesToRotate > 0:
+		rotate_room(spawnedRoom, timesToRotate)
+	
+	
+	# halls
+	#if surroundingRooms == 2 and temporaryRooms[x+1][z] != null and temporaryRooms[x-1][z] != null:
+		#rotate_room(temporaryRoom)
+	
+	# Bend rooms
+	#if surroundingRooms == 2 and temporaryRooms[x][z+1] != null
+		#if temporaryRooms[x+1][z]:
+			#
+		#
+		#if temporaryRooms[x-1][z]
+	
+	#if spawnedRoom.get_script() == null:
+		#push_error("Room has no script!")
+
+
+func rotate_room(room: Node3D, timesToRotate):
+	for i in timesToRotate:
+		room.rotate(Vector3.UP, deg_to_rad(-90))
